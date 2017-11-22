1bb324c434853107fd254ef1da2b7bb52ce8f3c2
piagarwal1106 <33057585+piagarwal1106@users.noreply.github.com>
2017-11-22 14:30:39 +0530

    Upgraded google apis (#5)
    Upgraded google apis

--- src/main/scala/com/crealytics/google/adwords/AdWordsClient.scala
+++ src/main/scala/com/crealytics/google/adwords/AdWordsClient.scala
@@ -4,10 +4,13 @@ import java.util.zip.GZIPInputStream
 import javax.xml.parsers.DocumentBuilderFactory
 
 import com.google.api.ads.adwords.axis.factory.AdWordsServices
-import com.google.api.ads.adwords.axis.v201702.cm.{ReportDefinitionField, ReportDefinitionReportType, ReportDefinitionServiceInterface}
+import com.google.api.ads.adwords.axis.v201710.cm.{ReportDefinitionField, ReportDefinitionServiceInterface}
+import com.google.api.ads.adwords.axis.v201710.cm.{ReportDefinitionField, ReportDefinitionReportType, ReportDefinitionServiceInterface}
 import com.google.api.ads.adwords.lib.client.AdWordsSession
-import com.google.api.ads.adwords.lib.jaxb.v201702.DownloadFormat
-import com.google.api.ads.adwords.lib.utils.v201702.ReportDownloader
+import com.google.api.ads.adwords.lib.jaxb.v201710.{DownloadFormat}
+import com.google.api.ads.adwords.lib.jaxb.v201710.DownloadFormat
+import com.google.api.ads.adwords.lib.utils.v201710.ReportDownloader
+import com.google.api.ads.adwords.lib.utils.v201710.ReportDownloader
 import com.google.api.ads.common.lib.auth.OfflineCredentials
 import com.google.api.client.auth.oauth2.Credential
 
